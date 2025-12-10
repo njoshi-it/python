9ee30401132c2ebff64aa6dfdd800dde02006d33
njoshi-it <joshinabin792@gmail.com>
2025-12-10 15:18:28 +0545

    installing first package with pip

--- try.py
+++ try.py
@@ -1,5 +1,7 @@
-import random
-def roll(): return random.randint(1, 6)
-def func() :
-    for r in iter(roll, 6):   # keep rolling until we get a 6 (6 is sentinel)
-    return r
+import camelcase
+
+c = camelcase.CamelCase()
+
+txt = "lorem ipsum dolor sit amet"
+
+print(c.hump(txt))
