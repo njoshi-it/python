13b24a9cab04ce1baf1d87ab21110dd8c450f452
njoshi-it <joshinabin792@gmail.com>
2025-12-11 07:31:28 +0545

    type hint added

---
+++ fast.py
@@ -0,0 +1,6 @@
+def get_full_name(first_name: str, last_name: str): #type hint 
+    full_name = first_name.title() + " " + last_name.title()
+    return full_name
+
+
+print(get_full_name("john", "doe"))
